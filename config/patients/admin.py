55ed96315d085957e2e645a2e0554b333756d188
sjtgs <chipilej@gmail.com>
2026-03-05 13:47:48 +0200

    Added Patient Admin

--- config/patients/admin.py
+++ config/patients/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Patient
 
-# Register your models here.
+@admin.register(Patient)
+class PatientAdmin(admin.ModelAdmin):
+    list_display = (
+        "patient_id",
+        "first_name",
+        "last_name",
+        "guardian",
+        "date_registered"
+    )
+
+    search_fields = (
+        "patient_id",
+        "first_name",
+        "last_name",
+    )
+
+    list_filter = ("date_registered",)
