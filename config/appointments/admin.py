2d58ea687632c74a431757d4e31a91ad037f1c87
sjtgs <chipilej@gmail.com>
2026-03-05 13:49:49 +0200

    Created Admin for Appointment and Doctor app

--- config/appointments/admin.py
+++ config/appointments/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Appointment
 
-# Register your models here.
+@admin.register(Appointment)
+class AppointmentAdmin(admin.ModelAdmin):
+    list_display = (
+        "patient",
+        "doctor",
+        "appointment_date",
+        "appointment_time",
+        "status"
+    )
+
+    list_filter = (
+        "appointment_date",
+        "doctor",
+        "status"
+    )
+
+    search_fields = (
+        "patient__first_name",
+        "patient__last_name",
+        "patient__patient_id"
+    )
--- config/doctors/admin.py
+++ config/doctors/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Doctor
 
-# Register your models here.
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ("name", "specialty", "is_active")
+    search_fields = ("name", "specialty")
